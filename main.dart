de9f079d8d11844f5c0628d09cf4a55334177265
Shuvojit <shuvojitkar241@gmail.com>
2018-12-17 22:07:16 +0600

    LINQ, Projection, Element

--- main.dart
+++ main.dart
@@ -6,10 +6,31 @@ main() {
 
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  WithoutLINQ(numbers);
-  WithLinQ(numbers);
-  customers.forEach(print);
+  //WithoutLINQ(numbers);
+  //WithLinQ(numbers);
+
+  var whereResult = customers.where((c) => c.customerName.contains("Shuvojit"));
+  //whereResult.forEach(print);
   //We can do this
+
+  //Projection Operartors
+  var projResult = customers.map((c) => c.customerName);
+  //projResult.forEach(print);
+
+  //Projection Operator  (Select many - Compound )
+  var R = customers
+      .expand((c) => c.Orders)
+      .where((o) => o.orderTotal > 2000)
+      .take(3);
+  //R.forEach(print);
+
+  //SetOperator
+  var num = [1, 2, 3];
+  var num2 = [1, 2, 6];
+  var intersec = num.toSet().intersection(num2.toSet());
+  intersec.forEach(print);
+  var union = num.toSet().union(num2.toSet());
+  union.forEach(print);
 }
 
 void WithoutLINQ(List numbers) {
